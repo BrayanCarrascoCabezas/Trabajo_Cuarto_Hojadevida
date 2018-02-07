035d48c457e273d17658f8199abb91ef347b9fcb
Davidssito Campos <Davidssito Campos@DESKTOP-S8KPSSD.local>
2018-02-06 19:08:58 -0500

    David Campos ITitulo_Pregado BN

--- Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/rnegocio/dao/ITitulo_Pregado.java
+++ Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/rnegocio/dao/ITitulo_Pregado.java
@@ -4,11 +4,17 @@
  * and open the template in the editor.
  */
 package trabajo_hoja_de_vida.rnegocio.dao;
-
+import trabajo_hoja_de_vida.rnegocio.entidades.*;
+import java.util.*;
 /**
  *
  * @author Davidssito Campos
  */
 public interface ITitulo_Pregado {
+    public int insertar(Titulo_Pregado titulo_pregado) throws Exception;
+    public int modificar(Titulo_Pregado titulo_pregado) throws Exception;
+    public int eliminar(Titulo_Pregado titulo_pregado) throws Exception;
+    public Titulo_Pregado obtener(int codigo) throws Exception;
+    public List<Titulo_Pregado> obtener() throws Exception; 
     
 }
