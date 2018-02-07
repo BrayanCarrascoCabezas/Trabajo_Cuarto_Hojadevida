0c1ef37e029c3fc0dd08b84ec23bce555d332e4e
ANDRES OROZCO <>
2018-02-07 00:01:35 -0500

    Creacios curso seminario

---
+++ Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/rnegocio/entidades/Curso_Seminario.java
@@ -0,0 +1,15 @@
+
+package trabajo_hoja_de_vida.rnegocio.entidades;
+
+
+public class Curso_Seminario {
+    
+    private Docente docente;
+    private int cod_CursoSeminario;
+    private String nombreCS;
+    private int horas;
+    private String ubicacion;
+    private String descripcion;
+    
+    
+}
