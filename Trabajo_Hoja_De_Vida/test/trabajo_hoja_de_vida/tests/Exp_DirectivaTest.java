18becd9f203370c421e1e2679d0828774d63304d
Edwin Guaman <Usuario@172.30.3.164>
2018-02-21 15:38:30 -0500

    Test Exp_Directiva

---
+++ Trabajo_Hoja_De_Vida/test/trabajo_hoja_de_vida/tests/Exp_DirectivaTest.java
@@ -0,0 +1,43 @@
+package trabajo_hoja_de_vida.tests;
+import trabajo_hoja_de_vida.rnegocio.dao.IExp_Directiva;
+import trabajo_hoja_de_vida.rnegocio.entidades.Exp_Directiva;
+import trabajo_hoja_de_vida.rnegocio.impl.Exp_DirectivaImpl;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exp_DirectivaTest {
+     public Exp_DirectivaTest() {
+    }
+    @Test
+    public void testGeneral(){
+        int filas=0;
+        IExp_Directiva expdirectivaDao=new Exp_DirectivaImpl();
+        
+       Exp_Directiva expdirectiva = new Exp_Directiva(7,3,"UNACH","PHD",(8/2/2018),(3/4/2019));
+        try {
+            filas=expdirectivaDao.insertar(expdirectiva);
+           System.out.println("Ingreso de " + filas + " Filas Correctas");
+       } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        assertEquals(filas>0, true);
+
+        List<Exp_Directiva> lista=new ArrayList<>();
+        try {
+            lista = expdirectivaDao.obtener();
+            for(Exp_Directiva ec:lista){
+                System.out.println(ec.getCodigoExp_D()+" "+ec.getCodigoE()+" "+ec.getEmpresa_Institucion()+" "+ec.getEntidades()+" "+ec.getFecha_Inicio()+" "+ec.getFecha_Final());
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            
+        }
+        assertTrue(lista.size()>0);
+    } 
+}
+    
+
+
