7c4369abfc0ba08e978278a89c4fa6855048770e
Usuario <Usuario@DESKTOP-17ILUSK>
2018-02-22 14:40:56 -0500

    MarcoAmancha

--- Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/rnegocio/impl/Exp_ProfecionalImpl.java
+++ Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/rnegocio/impl/Exp_ProfecionalImpl.java
@@ -7,7 +7,9 @@ import java.util.List;
 import trabajo_hoja_de_vida.accesodatos.Conexion;
 import trabajo_hoja_de_vida.accesodatos.Parametro;
 import trabajo_hoja_de_vida.rnegocio.dao.IExp_Profecional;
+import trabajo_hoja_de_vida.rnegocio.dao.IExperiencia;
 import trabajo_hoja_de_vida.rnegocio.entidades.Exp_Profecional;
+import trabajo_hoja_de_vida.rnegocio.entidades.Experiencia;
 
 public class Exp_ProfecionalImpl implements IExp_Profecional {
 
@@ -18,8 +20,8 @@ public class Exp_ProfecionalImpl implements IExp_Profecional {
                 + "(?,?,?,?,?,?)";
         List<Parametro> lstPar = new ArrayList<>();
         lstPar.add(new Parametro(1, exp_profecional.getCodigoExp()));
-        lstPar.add(new Parametro(2, exp_profecional.getCodigoE()));
-        lstPar.add(new Parametro(3, exp_profecional.getEmpresa_institucion()));
+        lstPar.add(new Parametro(2, exp_profecional.getExperiencia().getCodigoE()));
+        lstPar.add(new Parametro(3, exp_profecional.getEmpresa_nstitucion()));
         lstPar.add(new Parametro(4, exp_profecional.getPosicion()));
         lstPar.add(new Parametro(5, exp_profecional.getFecha_inicio()));
         lstPar.add(new Parametro(6, exp_profecional.getFecha_final()));
@@ -43,12 +45,12 @@ public class Exp_ProfecionalImpl implements IExp_Profecional {
     public int modificar(Exp_Profecional exp_profecional) throws Exception {
       int numFilasAfectadas = 0;
         String sql = "UPDATE exp_profecional"
-                + "   SET codigoExp=?, codigoE=?, empresa_institucion=?, posicion=?, fecha_inicio=?, fecha_final=? "
+                + "   SET codigoExp=?, codigoE=?, empresa_nstitucion=?, posicion=?, fecha_inicio=?, fecha_final=? "
                 + " where codigoExp=?";
         List<Parametro> lstPar = new ArrayList<>();
         lstPar.add(new Parametro(1, exp_profecional.getCodigoExp()));
-        lstPar.add(new Parametro(2, exp_profecional.getCodigoE()));
-        lstPar.add(new Parametro(3, exp_profecional.getEmpresa_institucion()));
+        lstPar.add(new Parametro(2, exp_profecional.getExperiencia().getCodigoE()));
+        lstPar.add(new Parametro(3, exp_profecional.getEmpresa_nstitucion()));
         lstPar.add(new Parametro(4, exp_profecional.getPosicion()));
         lstPar.add(new Parametro(5, exp_profecional.getFecha_inicio()));
         lstPar.add(new Parametro(6, exp_profecional.getFecha_final()));
@@ -92,7 +94,8 @@ public class Exp_ProfecionalImpl implements IExp_Profecional {
    @Override
     public Exp_Profecional obtener(int codigoExp) throws Exception {
         Exp_Profecional exp_profecional = null;
-        String sql = "SELECT * FROM exp_profecional where codigoExp=?;";
+        String sql = "SELECT codigoExp, codigoE, empresa_nstitucion, posicion, fecha_inicio,"
+                + "fecha_final FROM exp_profecional where codigoExp=?;";
         List<Parametro> lstPar = new ArrayList<>();
         lstPar.add(new Parametro(1, codigoExp));
         Conexion con = null;
@@ -105,12 +108,14 @@ public class Exp_ProfecionalImpl implements IExp_Profecional {
                
                 exp_profecional = new Exp_Profecional();
                 exp_profecional.setCodigoExp(rst.getInt(1));
-                exp_profecional.setCodigoE(rst.getInt(2));
-                exp_profecional.setEmpresa_institucion(rst.getString(3));
+                IExperiencia experienciadao = new ExperienciaImpl();
+                Experiencia experiencia = experienciadao.obtener(rst.getInt(2));;
+                exp_profecional.setEmpresa_nstitucion(rst.getString(3));
                 exp_profecional.setPosicion(rst.getString(4));
                 exp_profecional.setFecha_inicio(rst.getDate(5));
                 exp_profecional.setFecha_final(rst.getDate(6));
-
+                exp_profecional.setExperiencia(experiencia);
+                exp_profecional.setExperiencia(experiencia);
             }
         } catch (Exception e) {
             throw e;
@@ -125,7 +130,8 @@ public class Exp_ProfecionalImpl implements IExp_Profecional {
     @Override
     public List<Exp_Profecional> obtener() throws Exception {
         List<Exp_Profecional> lista = new ArrayList<>();;
-        String sql = "SELECT * FROM exp_profecional ";
+        String sql = "SELECT codigoExp, codigoE, empresa_nstitucion, posicion, fecha_inicio,"
+                + "fecha_final FROM exp_profecional ";
         Conexion con = null;
         try {
             con = new Conexion();
@@ -135,13 +141,13 @@ public class Exp_ProfecionalImpl implements IExp_Profecional {
             while (rst.next()) {
                 exp_profecional = new Exp_Profecional();
                 exp_profecional.setCodigoExp(rst.getInt(1));
-                exp_profecional.setCodigoE(rst.getInt(2));
-                exp_profecional.setEmpresa_institucion(rst.getString(3));
+                 IExperiencia experienciadao = new ExperienciaImpl();
+                Experiencia experiencia = experienciadao.obtener(rst.getInt(2));
+                exp_profecional.setExperiencia(experiencia);
+                exp_profecional.setEmpresa_nstitucion(rst.getString(3));
                 exp_profecional.setPosicion(rst.getString(4));
                 exp_profecional.setFecha_inicio(rst.getDate(5));
                 exp_profecional.setFecha_final(rst.getDate(6));
-                
-
                 lista.add(exp_profecional);
             }
         } catch (Exception e) {
