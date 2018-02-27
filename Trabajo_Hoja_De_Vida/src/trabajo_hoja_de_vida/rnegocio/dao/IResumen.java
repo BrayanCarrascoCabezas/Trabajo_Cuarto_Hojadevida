66bf744a71bda2af95a209bdd2c6ad4f6c15e11b
User <User@PC>
2018-02-27 09:04:16 -0500

    Actualizacion

--- Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/rnegocio/dao/IResumen.java
+++ Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/rnegocio/dao/IResumen.java
@@ -8,7 +8,7 @@ package trabajo_hoja_de_vida.rnegocio.dao;
 import trabajo_hoja_de_vida.rnegocio.entidades.*;
 import java.util.*;
 public interface IResumen {
-          public int insertar(Resumen resumen) throws Exception;
+    public int insertar(Resumen resumen) throws Exception;
     public int modificar(Resumen resumen) throws Exception;
     public int eliminar(Resumen resumen) throws Exception;
     public Resumen obtener(int cod_docente) throws Exception;
