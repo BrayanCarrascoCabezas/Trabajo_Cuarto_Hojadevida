14b44496f1f1fc724751b8308f26e50d537a4c84
FabricioConstante <Usuario@FI-LABC01-PC01>
2018-02-27 17:09:34 -0500

    CURSO

--- Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/vistas/FrmCursoLista.java
+++ Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/vistas/FrmCursoLista.java
@@ -4,19 +4,15 @@
  * and open the template in the editor.
  */
 package trabajo_hoja_de_vida.vistas;
-
-import java.awt.BorderLayout;
-import java.util.ArrayList;
-import java.util.List;
-import javax.swing.JInternalFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
 import trabajo_hoja_de_vida.rnegocio.dao.ICurso;
 import trabajo_hoja_de_vida.rnegocio.entidades.Curso;
 import trabajo_hoja_de_vida.rnegocio.impl.CursoImpl;
-
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author FabricioConstante
@@ -26,14 +22,15 @@ public class FrmCursoLista extends JInternalFrame{
     JTable tabla;
     DefaultTableModel modelo;
     public FrmCursoLista(){
-    this.setSize(800,600);
-    this.setLayout(new BorderLayout());
-    this.setClosable(true);
-    lblTitulo=new JLabel("Listado de Cursos");
-    tabla = new JTable();
-    this.add(lblTitulo,BorderLayout.NORTH);
-    this.add(tabla,BorderLayout.CENTER);
-    cargarTabla();
+        this.setSize(400,300);
+        this.setLayout(new BorderLayout());
+        this.setClosable(true);
+        this.setBackground(Color.getHSBColor(100,60,270).brighter());
+        lblTitulo=new JLabel("Listado de Cursos");
+        tabla = new JTable();
+        this.add(lblTitulo,BorderLayout.NORTH);
+        this.add(tabla,BorderLayout.NORTH);
+        cargarTabla();
     }
     public void cargarTabla(){
         modelo = new DefaultTableModel();
@@ -49,7 +46,7 @@ public class FrmCursoLista extends JInternalFrame{
         }
         
         for(Curso est:lista){
-            modelo.addRow(new Object[]{est.getCodigo(),est.getDescripcion().toString()});
+            modelo.addRow(new Object[]{est.getCodigo(),est.getDescripcion()});
         }
         tabla.setModel(modelo);
     }
