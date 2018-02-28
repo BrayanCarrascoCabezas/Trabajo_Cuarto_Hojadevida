d9f21f999fe5f1d64a24d7a73112c543a1565705
Edwin Guaman <admin@DESKTOP-O7UUV48>
2018-02-27 21:51:02 -0500

    Exp_Directiva Lista

--- Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/vistas/FrmListaExp_Directiva.java
+++ Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/vistas/FrmListaExp_Directiva.java
@@ -1,3 +1,4 @@
+
 package trabajo_hoja_de_vida.vistas;
 
 import java.awt.BorderLayout;
@@ -12,44 +13,48 @@ import javax.swing.table.DefaultTableModel;
 import trabajo_hoja_de_vida.rnegocio.dao.*;
 import trabajo_hoja_de_vida.rnegocio.entidades.*;
 import trabajo_hoja_de_vida.rnegocio.impl.*;
-public class FrmListaExp_Directiva extends JInternalFrame {
- JLabel lblTitulo;
+
+public class FrmListaExp_Directiva extends JInternalFrame{
+    JLabel lblTitulo;
     JTable tabla;
     DefaultTableModel modelo;
     JScrollPane jscTabla;
 
-    public FrmListaExp_Directiva(){
-    this.setSize(800,600);
+    public FrmListaExp_Directiva() {
+        this.setSize(800,600);
     this.setLayout(new BorderLayout());
     this.setClosable(true);
-    lblTitulo =new JLabel("Listado Exp Directiva ");
+    lblTitulo =new JLabel("Listado Experiencia Directiva ");
     tabla = new JTable();
     jscTabla= new JScrollPane(tabla); 
     this.add(lblTitulo, BorderLayout.NORTH);
     this.add(jscTabla, BorderLayout.CENTER);
     cargarTabla();
-    } 
-    public void cargarTabla(){
+    }
+    
+     public void cargarTabla(){
         modelo = new DefaultTableModel();
-        modelo.addColumn("CodigoExp_D");
-        modelo.addColumn("CodigoE");
-        modelo.addColumn("Empresa_Institucion");
-        modelo.addColumn("Entidades");
-        modelo.addColumn("Fecha_Inicio");
-         modelo.addColumn("Fecha_Final");
+        modelo.addColumn("Codigo: ");
+        modelo.addColumn("Experiencia: ");
+        modelo.addColumn("Empresa_Institucion: ");
+        modelo.addColumn("Entidades: ");
+        modelo.addColumn("Fecha de Inicio: ");
+         modelo.addColumn("Fecha Fin: ");
         
         List<Exp_Directiva>lista= new ArrayList<>();
         try {
-            IExp_Directiva excaDao = new Exp_DirectivaImpl();
-            lista = excaDao.obtener();
+            IExp_Directiva exdiDao = new Exp_DirectivaImpl();
+            lista = exdiDao.obtener();
         } catch (Exception e) {
             JOptionPane.showMessageDialog(this, e.getMessage(),"Error",
                     JOptionPane.ERROR_MESSAGE);
         }
         for(Exp_Directiva ec : lista){
-            modelo.addRow(new Object[]{ ec.getCodigoExp_D(),ec.getCodigoE(),ec.getEmpresa_Institucion(),ec.getEntidades()
+            modelo.addRow(new Object[]{ ec.getCodigoExp_D(),ec.getExperiencia().getCodigoE(),ec.getEmpresa_Institucion(),ec.getEntidades()
                     ,ec.getFecha_Inicio(),ec.getFecha_Final().toString()});
         }
         tabla.setModel(modelo);
     }
-    }
+    
+    
+}
