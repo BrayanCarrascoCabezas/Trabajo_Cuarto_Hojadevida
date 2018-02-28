faef254299ab0c32f5cdbbe04c3034ee926e3bed
Edwin Guaman <admin@DESKTOP-O7UUV48>
2018-02-27 21:02:06 -0500

    Exp_Directiva

--- Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/rnegocio/entidades/Exp_Directiva.java
+++ Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/rnegocio/entidades/Exp_Directiva.java
@@ -1,26 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package trabajo_hoja_de_vida.rnegocio.entidades;
 
 import java.sql.Date;
-import java.util.*;
 public class Exp_Directiva {
     private int CodigoExp_D;  
-    private int CodigoE;
-      private String Empresa_Institucion;
-      private String Entidades;
-      private Date Fecha_Inicio;
-      private Date Fecha_Final;
+    private Experiencia experiencia;
+    private String Empresa_Institucion;
+    private String Entidades;
+    private Date Fecha_Inicio;
+    private Date Fecha_Final;
 
     public Exp_Directiva() {
     }
 
-    public Exp_Directiva(int CodigoExp_D, int  CodigoE, String Empresa_Institucion, String Entidades, Date Fecha_Inicio, Date Fecha_Final) {
+    public Exp_Directiva(int CodigoExp_D, Experiencia experiencia, String Empresa_Institucion, String Entidades, Date Fecha_Inicio, Date Fecha_Final) {
         this.CodigoExp_D = CodigoExp_D;
-        this.CodigoE = CodigoE;
+        this.experiencia = experiencia;
         this.Empresa_Institucion = Empresa_Institucion;
         this.Entidades = Entidades;
         this.Fecha_Inicio = Fecha_Inicio;
@@ -34,12 +29,13 @@ public class Exp_Directiva {
     public void setCodigoExp_D(int CodigoExp_D) {
         this.CodigoExp_D = CodigoExp_D;
     }
-     public int  getCodigoE () {
-        return CodigoE;
+
+    public Experiencia getExperiencia() {
+        return experiencia;
     }
 
-    public void setCodigoE (int  CodigoE) {
-        this.CodigoE = CodigoE;
+    public void setExperiencia(Experiencia experiencia) {
+        this.experiencia = experiencia;
     }
 
     public String getEmpresa_Institucion() {
@@ -73,5 +69,5 @@ public class Exp_Directiva {
     public void setFecha_Final(Date Fecha_Final) {
         this.Fecha_Final = Fecha_Final;
     }
-  
+
 }
