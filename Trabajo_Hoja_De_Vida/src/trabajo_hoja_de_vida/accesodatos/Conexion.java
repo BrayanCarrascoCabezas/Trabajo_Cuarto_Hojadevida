698e2f8cf824ab0725fb570a0ccd47e1f60bc541
Usuario <Usuario@172.30.3.84>
2018-03-02 18:00:52 -0500

    TODO

--- Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/accesodatos/Conexion.java
+++ Trabajo_Hoja_De_Vida/src/trabajo_hoja_de_vida/accesodatos/Conexion.java
@@ -10,7 +10,7 @@ public class Conexion {
     public void conectar() {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            con = DriverManager.getConnection("jdbc:sqlserver://172.30.3.63:1433;databaseName=Trabajo_Hoja_De_Vida","usr_hvida", "123456");
+            con = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=HojadeVida","usr_h", "123");
         } catch (ClassNotFoundException e) {
             System.out.println("Error al cargar el driver: "
                     + e.getMessage());
